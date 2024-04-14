aa1ef090f2707e47758af4763440167211e4aad0
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-04-14 20:35:59 +0700

    Server action & useTransition

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -3,6 +3,7 @@ import { CardWrapper } from "@/components/auth/card-wrapper";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { startTransition, useTransition } from "react";
 
 import { LoginSchema } from "@/schemas";
 import { Input } from "@/components/ui/input";
@@ -17,8 +18,11 @@ import {
 } from "@/components/ui/form";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
+import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+  const [isPending, startTransition] = useTransition();
+
   // 1. Khai báo form với kiểu dữ liệu từ LoginSchema
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
@@ -30,7 +34,9 @@ export const LoginForm = () => {
 
   // 2. Khai báo hàm handleSubmit
   const handleSubmit = (values: z.infer<typeof LoginSchema>) => {
-    console.log(values);
+    startTransition(() => {
+      login(values)
+    });
   };
 
   return (
@@ -41,46 +47,52 @@ export const LoginForm = () => {
       showSocial
     >
       <Form {...form}>
-        <form 
-        onSubmit={form.handleSubmit(handleSubmit)}
-        className="space-y-6">
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
           <div className="space-y-4">
             <FormField
-            control={form.control}
-            name="email"
-            render={({field}) => (
+              control={form.control}
+              name="email"
+              render={({ field }) => (
                 <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                        <Input placeholder="abc@gmail.com" {...field} type="email"/>
-                    </FormControl>
-                    <FormMessage />
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                    disabled={isPending}
+                      placeholder="abc@gmail.com"
+                      {...field}
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
-            )} />
+              )}
+            />
 
             <FormField
-            control={form.control}
-            name="password"
-            render={({field}) => (
+              control={form.control}
+              name="password"
+              render={({ field }) => (
                 <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                        <Input placeholder="********" {...field} type="password"/>
-                    </FormControl>
-                    <FormMessage />
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input 
+                    disabled={isPending}
+                    placeholder="********" {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
-            )} />
-
+              )}
+            />
           </div>
 
-            <FormError message="" />
-            <FormSuccess message="" />
+          <FormError message="" />
+          <FormSuccess message="" />
 
-            <Button
-              type="submit"
-              className="w-full">
-              Login
-            </Button>
+          <Button 
+          disabled={isPending}
+          type="submit" className="w-full">
+            Login
+          </Button>
         </form>
       </Form>
       {
