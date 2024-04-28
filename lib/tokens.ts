42260fa35cceb50ee65020d5112386b406c1d9ab
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-04-28 14:47:33 +0700

    Get and generate two factor token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,34 @@
 'use server';
+import crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
 import { db } from '@/lib/db';
 import { getVerificationTokenByEmail } from '@/data/verification-token';
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
+
+export const generateTwoFactorToken = async (email: string) => {
+    const token = crypto.randomInt(100_000, 1_000_000).toString();
+
+    // TODO: Nhớ thay đổi thởi gian hết hạn token thành 15 phút
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+    const existingToken = await getTwoFactorTokenByEmail(email);
+
+    if (existingToken) {
+        await db.twoFactorToken.delete({
+            where: { id: existingToken.id },
+        });
+    }
+
+    const twoFactorToken = await db.twoFactorToken.create({
+        data: {
+            email,
+            token,
+            expires,
+        },
+    })
+
+    return twoFactorToken;
+}
 
 export const generatePasswordResetToken = async (email: string) => {
     const token = uuidv4();
