f8ceeeac8b09b058f67524e2a256af240034c3a4
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-06-09 22:47:01 +0700

    Sync data on server and client page when update setting

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,16 +1,46 @@
 "use client";
+import { useTransition } from "react";
 import { logout } from "@/actions/logout";
-import { useCurrentUser } from "@/hooks/use-current-user";
+import { settings } from "@/actions/setting";
+import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+} from "@/components/ui/card";
 
 const SettingPage = () => {
-  const user = useCurrentUser();
+  const { update } = useSession();
+  const [pending, startTransition] = useTransition();
+
+  const handleSubmit = () => {
+    startTransition(async () =>  {
+      await settings({
+        name: "Trung Kiên",
+      }).then(() => {
+        update();
+      });
+    });
+  };
 
   const onClick = () => {
     logout();
   };
   return (
     <div>
-      {JSON.stringify(user)}
+      <Card className="w-[600px]">
+        <CardHeader className="font-bold">
+          <p className="font-semibold text-2xl text-center">
+            🛠️Settings
+          </p>
+        </CardHeader>
+        <CardFooter>
+          <Button disabled={pending} onClick={handleSubmit}>Update</Button>
+        </CardFooter>
+      </Card>
       <button
         className="bg-white p-6 rounded-2xl"
         onClick={onClick}
--- auth.ts
+++ auth.ts
@@ -7,91 +7,102 @@ import { UserRole } from "@prisma/client";
 import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    pages: {
-        signIn: "/auth/login",
-        error: "/auth/error",
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
     },
-    events: {
-        async linkAccount({ user }) {
-            await db.user.update({
-                where: { id: user.id },
-                data: {
-                    emailVerified: new Date(),
-                },
-            });
-        },
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== "credentials") {
+        return true;
+      }
+
+      if (user.id) {
+        const existingUser = await getUserById(user.id);
+
+        // Prevent login without email verification
+        if (!existingUser?.emailVerified) {
+          return false;
+        }
+
+        // Two factor confirmation
+        if (existingUser.isTwoFactorEnabled) {
+          const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+            existingUser.id
+          );
+          if (!twoFactorConfirmation) return false;
+
+          // Delete two factor confirmation for next sign in
+          await db.twoFactorConfirmation.delete({
+            where: { id: twoFactorConfirmation.id },
+          });
+        }
+      }
+
+      return true;
     },
-    callbacks: {
-        async signIn({ user, account }) {
-            // Allow OAuth without email verification
-            if (account?.provider !== "credentials") {
-                return true;
-            }
+    // Ban đầu ta có JWT sau khi đăng nhập
+    // JWT bao gồm các thông tin cơ bản của User
+    async jwt({ token }) {
+      console.log({ token: token });
 
-            if (user.id) {
-                const existingUser = await getUserById(user.id);
+      // token.sub does not exit when you log out
+      if (!token.sub) return token;
 
-                // Prevent login without email verification
-                if (!existingUser?.emailVerified) {
-                    return false;
-                }
+      //  User does not exist in the database when you sign up
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
 
-                // Two factor confirmation
-                if (existingUser.isTwoFactorEnabled) {
-                    const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
-                    if (!twoFactorConfirmation)
-                        return false;
-                    
-                    // Delete two factor confirmation for next sign in
-                    await db.twoFactorConfirmation.delete({
-                        where: { id: twoFactorConfirmation.id },
-                    });
-                }
-            }
+      // User exists in the database when you log in
+      // Update username
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
-            return true;
-        },
-        // Ban đầu ta có JWT sau khi đăng nhập
-        // JWT bao gồm cá  c thông tin cơ bản của User
-        async jwt({ token }) {
-            console.log({ token: token });
-
-            // token.sub does not exit when you log out
-            if (!token.sub) return token;
-
-            //  User does not exist in the database when you sign up
-            const existingUser = await getUserById(token.sub);
-            if (!existingUser) return token;
-
-            // User exists in the database when you log in
-            token.role = existingUser.role;
-            token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
-
-            return token;
-        },
-
-        // Để tạo được một session cần có JWT (JSON Web Token)
-        async session({ session, token }) {
-            if (token.sub && session.user) {
-                session.user.id = token.sub;
-            }
-
-            if (token.role && session.user) {
-                session.user.role = token.role as UserRole;
-            }
-
-            if (session.user) {
-                session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as Boolean;
-            }
-
-            console.log({
-                session: session,
-                token: token,
-            });
-            return session;
-        },
+      return token;
     },
-    adapter: PrismaAdapter(db),
-    session: { strategy: "jwt" },
-    ...authConfig,
+
+    // Để tạo được một session cần có JWT (JSON Web Token)
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as Boolean;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        if (token.email) {
+          session.user.email = token.email;
+        }
+      }
+
+      console.log({
+        session: session,
+        token: token,
+      });
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
